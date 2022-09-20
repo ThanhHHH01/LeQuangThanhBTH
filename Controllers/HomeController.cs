28d2cc04de4bb1cb7570484feebf7a2bce5cde5a
ThanhHHH01 <112218811+ThanhHHH01@users.noreply.github.com>
2022-09-20 15:43:07 +0700

    make it better

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,41 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Demo.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using LeQuangThanhBTH.Models;
 
-namespace Demo.Controllers;
-
-public class HomeController : Controller
+namespace LeQuangThanhBTH.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class EmployeeController : Controller
     {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            // khoi tao 1 list Employee 
+            List<Employee> stdList = new List<Employee>()
+            {
+                new Employee{EmployeeID = 1, EmployeeName="LeQuangThanh", EmployeeAge = 19},
+                new Employee{EmployeeID = 2, EmployeeName="LeQuangThanhh", EmployeeAge = 19},
+                new Employee{EmployeeID = 3, EmployeeName="LeQuangThanhhh", EmployeeAge = 19},
+                new Employee{EmployeeID = 4, EmployeeName="LeQuangThanhhhh", EmployeeAge = 19},
+                new Employee{EmployeeID = 5, EmployeeName="LeQuangThanhhhhh", EmployeeAge = 19}
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+            };
+            ViewData["Employee"] = stdList;
+            return View(stdList);
+        }
+        [HttpPost]
+        //     public IActionResult Index(string EmployeeID, string EmployeeName, string EmployeeAge)
+        //     {
+        //         // ViewBag.mess = std.EmployeeID + "-" + std.EmployeeName + "-" + std.EmployeeAge;
+        //         ViewBag.mess = "Mã sinh viên: " + EmployeeID + " ; " + "  Tên sinh viên: " + EmployeeName + " ; " + "Tuổi: " + EmployeeAge;
+        //         return View();
+        //     }
+        // }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Create(Employee std)
+        {
+            string message = std.EmployeeID + "-";
+            message += std.EmployeeName + "-";
+            message += std.EmployeeAge;
+            ViewBag.TT = message;
+            return View();
+        }
     }
 }
